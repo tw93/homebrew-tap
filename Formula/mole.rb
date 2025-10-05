b25075e06e447eddb75db2337e6adf30ae4aa7c3
Tw93 <hitw93@gmail.com>
2025-10-05 16:48:41 +0800

    mole V1.5.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mole.rb
+++ Formula/mole.rb
@@ -1,8 +1,8 @@
 class Mole < Formula
   desc "Comprehensive macOS cleanup and application uninstall tool"
   homepage "https://github.com/tw93/mole"
-  url "https://github.com/tw93/Mole/archive/refs/tags/V1.5.1.tar.gz"
-  sha256 "6c61249f11c0ee29da0bc362200eaa1262df83d1dd4a71d0d0c6daa5531c57a0"
+  url "https://github.com/tw93/Mole/archive/refs/tags/V1.5.2.tar.gz"
+  sha256 "41488fa387d5c1e4778f8d934c8a6d9333ecc1ef2ab5f284523a7fdbfcf10afb"
   license "MIT"
   head "https://github.com/tw93/mole.git", branch: "main"
 
