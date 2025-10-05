b43316c81864f026248578411c2d3e29cf341391
Tw93 <tw93@qq.com>
2025-10-05 20:40:26 +0800

    :bug: update mo

--- Formula/mole.rb
+++ Formula/mole.rb
@@ -20,11 +20,24 @@ class Mole < Formula
               "SCRIPT_DIR=\"#{libexec}\""
 
     bin.install "mole"
+
+    # Install mo alias (short command)
+    inreplace "mo",
+              'SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"',
+              "SCRIPT_DIR=\"#{bin}\""
+
+    bin.install "mo"
   end
 
   def caveats
     <<~EOS
       Mole is a macOS cleanup tool that requires administrative privileges for some operations.
+
+      You can use either 'mole' or 'mo' command:
+        mo                # Interactive menu
+        mo clean          # System cleanup
+        mo uninstall      # Remove applications
+        mo analyze        # Disk space explorer
 
       Update functionality is disabled when installed via Homebrew.
       To update, use: brew upgrade mole
@@ -33,5 +46,6 @@ class Mole < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/mole --version")
+    assert_match "Mole", shell_output("#{bin}/mo --help")
   end
 end
