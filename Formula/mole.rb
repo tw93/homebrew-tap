702b931ca7f720b98b34b47623e0c60457770db8
Tw93 <hitw93@gmail.com>
2025-12-29 20:24:49 +0800

    mole 1.15.9
    Automated release via GitHub Actions

--- Formula/mole.rb
+++ Formula/mole.rb
@@ -1,8 +1,8 @@
 class Mole < Formula
   desc "Comprehensive macOS cleanup and application uninstall tool"
   homepage "https://github.com/tw93/mole"
-  url "https://github.com/tw93/Mole/archive/refs/tags/V1.15.8.tar.gz"
-  sha256 "bdb19c40ad3d30b69555a904fe4b76c62b4118e4dea279e2b1cd03847ad63aa9"
+  url "https://github.com/tw93/Mole/archive/refs/tags/V1.15.9.tar.gz"
+  sha256 "9be0a9ffae60778ace2028fdf26e34e65b19d8b83eaa0082aefa76131c0807a9"
   license "MIT"
   head "https://github.com/tw93/mole.git", branch: "main"
 
