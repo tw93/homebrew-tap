b0cedc82b6f7dfee0482215e931176ce5709b7c9
tw93 <tw93@users.noreply.github.com>
2026-02-16 14:28:32 +0800

    kakuku 0.3.0

--- Casks/kakuku.rb
+++ Casks/kakuku.rb
@@ -1,6 +1,6 @@
 cask "kakuku" do
-  version "0.2.0"
-  sha256 "9dd01fac2df72578ca0ecb243cbf3c7964a6b8c9701c6eabcd57cfef70d4ec72"
+  version "0.3.0"
+  sha256 "16c8c3e39a116b389d3d9b43cd91746e7565aeaab9e49b0a830c5981bb25623c"
 
   url "https://github.com/tw93/Kaku/releases/download/V#{version}/Kaku.dmg",
       verified: "github.com/tw93/Kaku/"
